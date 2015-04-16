67f93da95995279d0c901eb5c8da5f132ddf02d9
America Calleros <america.quintero@gmail.com>
2015-04-16 14:25:35 -0500

    Add relationship between elections and ballots

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -1,3 +1,4 @@
 class Ballot < ActiveRecord::Base
   attr_accessible :election_id, :time
+  belongs_to :elections, dependent: :destroy
 end
--- app/models/election.rb
+++ app/models/election.rb
@@ -1,5 +1,6 @@
 class Election < ActiveRecord::Base
   attr_accessible :contact, :description, :end, :owner, :start, :title, :updid
   has_many :questions
+  has_many :ballots
   belongs_to :users
 end
