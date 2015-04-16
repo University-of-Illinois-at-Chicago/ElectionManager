81e297bde6f9eba852112313e107d0e80244ac29
America Calleros <america.quintero@gmail.com>
2015-04-16 16:28:32 -0500

    Create welcome controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ElectionManager::Application.routes.draw do
+  get "welcome/index"
+
   resources :votes
 
   resources :voters
@@ -64,7 +66,7 @@ ElectionManager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'elections#main'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
