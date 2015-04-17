44b16ebb7cddb5c937b1e9d3b4d83ad3e6574c36
America Calleros <america.quintero@gmail.com>
2015-04-17 15:28:12 -0500

    Add comments about apache configuration

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module ElectionManager
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #config.action_controller.relative_url_root = '/Elections'
   end
 end
