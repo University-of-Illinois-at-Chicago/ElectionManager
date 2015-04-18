df9e36e40785a70b638d24093ebb1dc759e96c1e
America Calleros <america.quintero@gmail.com>
2015-04-17 19:26:38 -0500

    Add start and end validation

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,6 +1,8 @@
 class Election < ActiveRecord::Base
   attr_accessible :contact, :description, :end, :start, :title, :updid, :user_id
   validates :contact, :presence => true
+  validates :end, :presence => true
+  validates :start, :presence => true
   validates :title, :presence => true
   has_many :ballots
   has_many :questions
