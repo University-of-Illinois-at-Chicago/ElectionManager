677a8c93e70754ca2ddf0b1a47c7dcb9b09699e6
America Calleros <america.quintero@gmail.com>
2015-05-01 15:27:27 -0500

    Set election_user table defaults

--- db/migrate/20150416172322_create_election_users.rb
+++ db/migrate/20150416172322_create_election_users.rb
@@ -1,12 +1,11 @@
 class CreateElectionUsers < ActiveRecord::Migration
   def change
     create_table :election_users do |t|
-      t.integer :election_id
-      t.integer :user_id
-      t.boolean :edit
-      t.boolean :view_results
-      t.string :updid
-
+      t.integer :election_id , :null => false
+      t.integer :user_id , :null => false
+      t.boolean :edit , :null => false, :default => 0
+      t.boolean :view_results, :null => false, :default => 0
+      t.string :updid, :null => false
       t.timestamps
     end
   end
