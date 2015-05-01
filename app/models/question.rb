fb654933205fd20ffe5038acfdd701199653fedb
America Calleros <america.quintero@gmail.com>
2015-05-01 13:52:11 -0500

    Add question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ActiveRecord::Base
   attr_accessible :election_id, :limit, :question, :updid, :write_in
   belongs_to :election, :class_name => "Election", :foreign_key => "election_id", dependent: :destroy
-  has_many :answer
+  has_many :answers
+  validates :question, :presence => true
+
 end
