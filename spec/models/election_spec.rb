331c3d12d41b989a194558101b919d6ba5034516
America Calleros <america.quintero@gmail.com>
2015-07-16 14:31:07 -0500

    Add spec files

---
+++ spec/models/election_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Election, :type => :model do
+
+  pending "able to create an election"
+  pending "require title"
+  pending "require contact"
+  pending "require start"
+  pending "require finish"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+end
