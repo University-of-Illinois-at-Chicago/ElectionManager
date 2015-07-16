7a056635163fe74b9a09e48a35a004e94e3d4f5b
America Calleros <america.quintero@gmail.com>
2015-07-16 15:54:30 -0500

    Add config files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectionManager::Application.config.secret_token = '4802d9176f2315a3f0e5484f73ca27672a88a3fd497eb322cfd84f4164f051466362d34d9f720d5ba77cbc6765ed2fca2d7a6bfebb69964bf1f0d942b34539b1'
