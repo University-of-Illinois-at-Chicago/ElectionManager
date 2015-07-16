301428e949ccfa893cceed65e5678ecfd9fa4ad9
America Calleros <america.quintero@gmail.com>
2015-07-16 17:07:33 -0500

    Create Elections model

--- db/migrate/20150716220253_create_elections.rb
+++ db/migrate/20150716220253_create_elections.rb
@@ -1,4 +1,4 @@
-class CreateElectionTable < ActiveRecord::Migration
+class CreateElections < ActiveRecord::Migration
   def change
     create_table :elections do |t| 
       t.string :title, :default => "", :null => false
