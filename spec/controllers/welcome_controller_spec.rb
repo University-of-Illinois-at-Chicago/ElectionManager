321d90af37a31f4b02444dd2dc470b0ee1a9b3b4
America Calleros <america.quintero@gmail.com>
2015-07-17 10:59:16 -0500

    Add spec files for unit testing

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
-
+  pending "Nothing to test in Welcome Controller so far"
 end
--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Election, :type => :model do
+  pending "something new"
   pending "add some examples to (or delete) #{__FILE__}"
 end
