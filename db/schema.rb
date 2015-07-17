fa694b231488e1de85e1fda0d511bdb5e3488de6
America Calleros <america.quintero@gmail.com>
2015-07-16 20:40:47 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150716215842) do
+ActiveRecord::Schema.define(:version => 20150716220253) do
 
   create_table "elections", :force => true do |t|
     t.string   "title",       :default => "", :null => false
